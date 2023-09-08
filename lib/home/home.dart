1500f6c02176d96a6c1f5c02d2136f1c000916b8
Song Yang <songyang.dev@gmail.com>
2023-09-07 23:12:18 -0400

    Dynamic text

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -25,10 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "You have clicked this many times:",
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
+            MotivationalText(count: _counter),
             Text(
               "$_counter",
               style: Theme.of(context).textTheme.headlineSmall,
@@ -45,6 +42,31 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class MotivationalText extends StatelessWidget {
+  const MotivationalText({super.key, required, required this.count});
+
+  final int count;
+
+  String _computeText(int count) {
+    return switch (count) {
+      0 => "Click me!",
+      1 => "Good",
+      int count when count >= 2 && count < 10 => "Keep going.",
+      int count when count >= 10 && count < 20 => "Don't give up!",
+      int count when count >= 20 => "Reset!",
+      _ => "Impossible!",
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _computeText(count),
+      style: Theme.of(context).textTheme.headlineSmall,
     );
   }
 }
