1c57a23f557c564c6dcde5d18f20c4990ac63cef
Song Yang <songyang.dev@gmail.com>
2023-09-07 22:59:21 -0400

    Add reset button

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({
     super.key,
   });
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +19,30 @@ class HomeScreen extends StatelessWidget {
         title: const Text("Clicking Game"),
       ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {},
-      ),
+          child: const Icon(Icons.add),
+          onPressed: () => setState(() => _counter++)),
       body: Center(
-        child: Text(
-          "You have clicked this many times:",
-          style: Theme.of(context).textTheme.headlineSmall,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "You have clicked this many times:",
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            Text(
+              "$_counter",
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            ElevatedButton.icon(
+              onPressed: () => setState(
+                () {
+                  _counter = 0;
+                },
+              ),
+              icon: const Icon(Icons.restore),
+              label: const Text("Reset"),
+            ),
+          ],
         ),
       ),
     );
